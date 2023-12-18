c0bf095ee687a51a17d1135cf6c0a8f7baea61ce
eka-septian <118880703+eka-septian@users.noreply.github.com>
2023-12-18 19:51:21 +0700

    Add stories model, migration & controller

---
+++ app/Http/Controllers/StoriesController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Stories;
+use Illuminate\Http\Request;
+
+class StoriesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Stories  $stories
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Stories $stories)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Stories  $stories
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Stories $stories)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Stories  $stories
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Stories $stories)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Stories  $stories
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Stories $stories)
+    {
+        //
+    }
+}
