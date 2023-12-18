6ea98fc340325e6db14a3740e2a9b43004e8e341
eka-septian <118880703+eka-septian@users.noreply.github.com>
2023-12-18 20:47:35 +0700

    Create user-stories relation

--- app/Models/Stories.php
+++ app/Models/Stories.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Stories extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'image',
+        'description'
+    ];
+
+    function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
